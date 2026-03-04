a60f5cad1558abeb6c67140321fd7807bc2de5a7
Moop333 <Moop3334@gmail.com>
2026-03-04 14:10:51 -0800

    Implemented a skeleton for the restaurant Class

--- project/backend/app/schema/resturant.py
+++ project/backend/app/schema/resturant.py
@@ -1,1 +1,21 @@
-#defines the methods related to how data is validated/stored, the model in MVC architecture
+from pydantic import BaseModel
+from typing import List
+import datetime
+
+class Resturant(BaseModel):
+    id: int
+    name: str
+    address: str
+    openTimes = List[datetime.datetime] = []
+    closeTimes = List[datetime.datetime] = []
+    #openDays has been removed in favour of consolidating it into openTimes/closeTimes using the datetime class
+
+class ItemCreate(BaseModel):
+    title: str
+    category: str
+    tags: List[str] = []
+
+class ItemUpdate(BaseModel):
+    title : str
+    category:str
+    tags: List[str] = []
