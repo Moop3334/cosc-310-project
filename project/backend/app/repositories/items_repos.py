9ed541d4058f1f5317a5991b545558f99b2cc815
Moop333 <Moop3334@gmail.com>
2026-03-10 18:22:10 -0700

    Finished the load deliveries method

--- project/backend/app/repositories/items_repos.py
+++ project/backend/app/repositories/items_repos.py
@@ -2,19 +2,26 @@
 # NEED TO DECIDE HOW TO STORE DATA
 
 from pathlib import Path
-import json, os
+import csv, os
 from typing import List, Dict, Any
 
-DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "items.json"
+DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "food_delivery.csv"
 
-def load_all() -> List[Dict[str, Any]]:
+#Returnes a list of dicts where each dict maps to an order
+def load_all_deliveries() -> List[Dict[Any]]:
    if not DATA_PATH.exists():
        return []
-   with DATA_PATH.open("r", encoding="utf-8") as f:
-       return json.load(f)
+   with DATA_PATH.open("r", encoding="utf-8", newline='') as f:
+       #TODO: Load specific data from csv
+       reader = csv.DictReader(f, delimiter=',')
+       orders = {}
+       #TODO:Add date class for validation
+       for row in reader:
+           orders[row.get("order_id")] = row
+       return orders
 
 def save_all(items: List[Dict[str, Any]]) -> None:
     tmp = DATA_PATH.with_suffix(".tmp")
     with tmp.open("w", encoding="utf-8") as f:
-        json.dump(items, f, ensure_ascii=False, indent=2)
-    os.replace(tmp, DATA_PATH)
+        #json.dump(items, f, ensure_ascii=False, indent=2)
+        os.replace(tmp, DATA_PATH)
