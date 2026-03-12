5431a7eeab1566e59d65bc4f78182b8fe8eeb7d7
YugGull <yug.gedu@gmail.com>
2026-03-12 03:10:18 -0700

    service class can validate cards

--- project/backend/app/services/service.py
+++ project/backend/app/services/service.py
@@ -1,1 +1,19 @@
-#buisness logic scripts go here, the controller element of the program
+#Can be further improved later will need access to the order.py file to import total from
+
+from order import Total;
+
+def validateCard(cardNumber, expiryDate, cvv):
+    
+    if(cardNumber.length==16 and expiryDate.length==4 and cvv.length==3):
+        return True
+    else:
+        return False  
+    
+def pay(user, Total):
+    
+    if(user.card==null){
+        print("No card on file, please enter card")
+        userAddCard();
+    }
+
+    if
