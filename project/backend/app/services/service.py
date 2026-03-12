9b52dfa8cdb3653439bca15de93c505fb72da99c
YugGull <yug.gedu@gmail.com>
2026-03-12 13:33:23 -0700

    Added

--- project/backend/app/services/service.py
+++ project/backend/app/services/service.py
@@ -1,19 +1,37 @@
 #Can be further improved later will need access to the order.py file to import total from
+import datetime
+from order import Order
+from pydantic import BaseModel
 
-from order import Total;
+class Payment(BaseModel):
+    id: int
+    total: float
+    card_number: int
+    expiry_date: datetime
+    cvv: int
+    #Should import from user class in future
 
-def validateCard(cardNumber, expiryDate, cvv):
-    
-    if(cardNumber.length==16 and expiryDate.length==4 and cvv.length==3):
-        return True
-    else:
-        return False  
-    
-def pay(user, Total):
-    
-    if(user.card==null){
-        print("No card on file, please enter card")
-        userAddCard();
-    }
+    def validateCard(cardNumber, expiryDate, cvv):
+        if(cardNumber.length==16 and expiryDate.length==4 and cvv.length==3):
+            return True
+        else:
+            return False
 
-    if
+    def authorisePayment(total):
+        if(total >= 0):
+            return True
+        else:
+            return False
+
+dummy_payment_info = {
+    'id': 101,
+    'total': 40.99,
+    'card_number': 1111222233334444,
+    'expiry_date': "16/10/2024",
+    'cvv': 123
+}
+
+try:
+    User(dummy_payment_info)
+except ValidationError as e:
+    print(e.errors())
