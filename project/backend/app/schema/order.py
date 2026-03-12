585d68390416dc84b3950a6e4767c4508e36b00d
SoftChipBag <qj0910@gmail.com>
2026-03-12 10:30:07 -0700

    Fixed class import issue, added OrderStatus class.

--- project/backend/app/schema/order.py
+++ project/backend/app/schema/order.py
@@ -1,17 +1,27 @@
-from menuItems import MenuItem
-from restaurant import Restaurant
+from orderDetail import OrderItem
 from customer import Customer
+from resturant import Restaurant
 from typing import List
 import datetime
 from pydantic import BaseModel, Field
 
+class OrderStatus(str, Enum):
+    PENDING = "pending approval"
+    PREPARING = "preparing order"
+    DELIVERING = "out for delivery"
+    DELIVERED = "delivered"
+    CANCELLED = "cancelled"
+#Can be used to create update status methods in other classes
+#Should hopefully help to keep status messages consistent when updating across classes
+
 class Order(BaseModel):
     id: int
     customer: Customer
+    restaurant: Restaurant
     items: List[OrderItem] = Field(default_factory=list)
     creation_date: datetime = datetime.now()
-    status: str = "pending approval"
-    #Will add method to update status later
+    status: OrderStatus = OrderStatus.PENDING
+    
 
     def calculateTotal(self):
         return sum(item.calculateSubTotal() for item in self.items) * 1.05
