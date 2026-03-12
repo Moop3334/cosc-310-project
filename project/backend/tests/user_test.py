02c5ec02b15bf4e2916e561a05d33f74a9c0ddf3
Yuya Sasaki <yuyasasaki@Yuyas-MacBook-Air.local>
2026-03-12 13:56:28 -0700

    Fix import in user tests

--- project/backend/tests/user_test.py
+++ project/backend/tests/user_test.py
@@ -1,15 +1,11 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from schema.user import User
+import pytest
+from app.schema.user import User
 
 
 def test_user_getters():
-    user = User(1, "Yuya", "123-456-7890", "123 Main St")
+    user = User(1, "Dave", "123-456-7890", "123 Main St")
     assert user.get_id() == 1
-    assert user.get_name() == "Yuya"
+    assert user.get_name() == "Dave"
     assert user.get_phone_number() == "123-456-7890"
     assert user.get_address() == "123 Main St"
 
