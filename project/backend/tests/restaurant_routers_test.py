51eb8b21569f20e99af2824b4fb557708cbc43dd
Moop333 <Moop3334@gmail.com>
2026-03-24 18:09:18 -0700

    Implemented some tests which pass successfully

--- project/backend/tests/restaurant_routers_test.py
+++ project/backend/tests/restaurant_routers_test.py
@@ -1,7 +1,11 @@
 import pytest
+import json
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from datetime import time
 from app.routers.restaurant_routers import router as restaurant_router, menu_router
+from app.services.restaurant_service import list_restaurants
+from app.schema.resturant import Restaurant
 
 app = FastAPI()
 
@@ -12,8 +16,18 @@ client = TestClient(app)
 
 #Restaurant Router Tests
 
-def test_get_restaurant_list():
-    assert True
+def test_get_restaurant_list(): #NOTE: There is almost certainly a better way to do this, but this works and won't impact the runtime of the actual website
+    response = client.get("/restaurants")
+    restaurants = list_restaurants()
+    assert response.status_code == 200
+    for r in range(1, len(response.json())):
+        tmp = restaurants[r].__dict__
+        for t in range(0, len(tmp["close_times"])):
+            tmp["close_times"][t] = time.isoformat(tmp["close_times"][t])
+            tmp["open_times"][t] = time.isoformat(tmp["open_times"][t])
+        for m in range(0, len(tmp["menu"])):
+            tmp["menu"][m] = tmp["menu"][m].__dict__
+        assert tmp == response.json()[r]
 
 def test_create_restaurant():
     assert True
