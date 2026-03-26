0d56e057b319d0bd1fbb506ae8de42fa44367e9e
Moop333 <Moop3334@gmail.com>
2026-03-25 23:19:45 -0700

    Resolved conflict in main.py

--- project/backend/app/main.py
+++ project/backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routers.restaurant_routers import router as restaurant_router, menu_router
 from app.routers.order_routers import router as order_router
+from app.routers.payment_routers import router as payment_router
+from app.routers.user_routers import router as user_router
 
 app = FastAPI()
 
@@ -16,3 +18,5 @@ def health():
 app.include_router(restaurant_router)
 app.include_router(menu_router)
 app.include_router(order_router)
+app.include_router(payment_router)
+app.include_router(user_router)
