8afdfcc0b7a08607bfaeed436de785a153a38953
Moop333 <Moop3334@gmail.com>
2026-04-07 13:41:21 -0700

    Updated the shopping cart schema to allow for different quantities of each item

--- project/backend/app/schema/shopping_cart.py
+++ project/backend/app/schema/shopping_cart.py
@@ -5,5 +5,11 @@ from app.schema.menu_items import MenuItem
 class ShoppingCart(BaseModel):
     user_id: NonNegativeInt
     restaurant_id: NonNegativeInt
-    items: List[MenuItem]
-    total: PositiveFloat
+    items: List[CartItem] = Field(default_factory=list)
+    total: PositiveFloat = Field(default=0.0)
+
+class CartItem(BaseModel):
+    item_id: NonNegativeInt
+    item_name: str = Field(min_length=1)
+    quantity: NonNegativeInt = Field(ge=1)
+    price: PositiveFloat
