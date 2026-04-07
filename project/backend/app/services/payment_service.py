350b1b03c2e9b5939f52dd659155d609edd7c292
Moop333 <Moop3334@gmail.com>
2026-04-07 15:11:40 -0700

    updated payment service to just take a list of cart items for each function instead of an order id

--- project/backend/app/services/payment_service.py
+++ project/backend/app/services/payment_service.py
@@ -2,11 +2,9 @@ from typing import List
 from fastapi import HTTPException
 from app.schema.payment import Payment
 from app.schema.shopping_cart import CartItem
-from app.services.order_service import get_specific_order
 
 def calculate_subtotal(cart: List[CartItem]) -> float:
     return sum(item.price * item.quantity for item in cart)
 
-def calculate_total(order_id: int) -> float:
-    order = get_specific_order(order_id)
-    return (calculate_subtotal(order.items) * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
+def calculate_total(cart: List[CartItem]) -> float:
+    return (calculate_subtotal(cart) * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
