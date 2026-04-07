80edc51feef72e374d0059c3d5b255a2027ab771
Moop333 <Moop3334@gmail.com>
2026-04-07 13:48:13 -0700

    updated the order schema to use cart items instead of a singular menu item

--- project/backend/app/schema/shopping_cart.py
+++ project/backend/app/schema/shopping_cart.py
@@ -1,6 +1,5 @@
 from typing import List
 from pydantic import BaseModel, Field, NonNegativeInt, PositiveFloat
-from app.schema.menu_items import MenuItem
 
 class ShoppingCart(BaseModel):
     user_id: NonNegativeInt
