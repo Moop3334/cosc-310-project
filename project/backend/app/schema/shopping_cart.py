66d19c747112008c4cf4b7633841a8dc0bffc814
Moop333 <Moop3334@gmail.com>
2026-04-07 13:26:13 -0700

    Implemented the shopping cart class

--- project/backend/app/schema/shopping_cart.py
+++ project/backend/app/schema/shopping_cart.py
@@ -1,4 +1,8 @@
 from typing import List
-import datetime
-from pydantic import BaseModel, Field, NonNegativeInt
+from pydantic import BaseModel, Field, NonNegativeInt, PositiveFloat
 from app.schema.menu_items import MenuItem
+
+class ShoppingCart(BaseModel):
+    restaurant_id: NonNegativeInt
+    items: List[MenuItem]
+    total: PositiveFloat
