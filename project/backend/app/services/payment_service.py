5c1decc3e3fe6783c3673539d9ba51993464b5ec
Moop333 <Moop3334@gmail.com>
2026-04-07 13:48:56 -0700

    Reworked payment services to work with multiple items

--- project/backend/app/services/payment_service.py
+++ project/backend/app/services/payment_service.py
@@ -2,6 +2,10 @@ from fastapi import HTTPException
 from app.schema.payment import Payment
 from app.services.order_service import get_specific_order
 
-def calculate_total(order_id: int) -> float:
+def calculate_subtotal(order_id: int) -> float:
     order = get_specific_order(order_id)
-    return (order.price * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
+    cart = order.items
+    return sum(item.price * item.quantity for item in cart)
+
+def calculate_total(order_id: int) -> float:
+    return (calculate_subtotal(order_id) * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
