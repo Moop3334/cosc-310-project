0023c41462da6052577c6c5f3d66a8a63299b6a1
Moop333 <Moop3334@gmail.com>
2026-04-07 15:10:02 -0700

    updated payment routers to prevent a circular import

--- project/backend/app/routers/payment_routers.py
+++ project/backend/app/routers/payment_routers.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from app.schema.payment import Payment
 from app.services.payment_service import calculate_total
+from app.services.order_service import get_specific_order
 
 router = APIRouter(prefix="/payments", tags=["payments"])
 
 @router.get("/{order_id}/total", response_model=float)
 def get_total(order_id: str):
-    return calculate_total(order_id)
+    return calculate_total(get_specific_order(order_id).items)
