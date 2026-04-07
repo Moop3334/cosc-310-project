db8539c21a3905616606ae2f27597224046514ac
Moop333 <Moop3334@gmail.com>
2026-04-07 14:05:06 -0700

    Updated payment services to allow subtotal calculations to work with shopping carts

--- project/backend/app/services/payment_service.py
+++ project/backend/app/services/payment_service.py
@@ -1,11 +1,12 @@
+from typing import List
 from fastapi import HTTPException
 from app.schema.payment import Payment
+from app.schema.shopping_cart import CartItem
 from app.services.order_service import get_specific_order
 
-def calculate_subtotal(order_id: int) -> float:
-    order = get_specific_order(order_id)
-    cart = order.items
+def calculate_subtotal(cart: List[CartItem]) -> float:
     return sum(item.price * item.quantity for item in cart)
 
 def calculate_total(order_id: int) -> float:
-    return (calculate_subtotal(order_id) * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
+    order = get_specific_order(order_id)
+    return (calculate_subtotal(order.items) * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
