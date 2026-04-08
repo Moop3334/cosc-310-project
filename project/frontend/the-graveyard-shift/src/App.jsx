6e43116d96450bb18b05e9247e1cac8df77b02e4
Moop333 <Moop3334@gmail.com>
2026-04-08 12:10:15 -0700

    Updated the restaurant browser url in the frontend to require a user be logged in before accessing

--- project/frontend/the-graveyard-shift/src/App.jsx
+++ project/frontend/the-graveyard-shift/src/App.jsx
@@ -7,15 +7,24 @@ import SignupPage from "./components/users/SignupPage";
 import HomePage from "./components/homepage/HomePage";
 import AdminPage from "./components/admin/AdminPage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import UserRoute from './components/UserRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/restaurants" element={<RestaurantBrowser />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
+        <Route 
+          path="/restaurants" 
+          element={
+            <UserRoute>
+              <RestaurantBrowser />
+            </UserRoute>
+          } 
+        />
 
         <Route
           path="/admin"
