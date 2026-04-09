a9d11a059cfd5786e06aa2c9b5bab45e3fccc2cb
Moop333 <Moop3334@gmail.com>
2026-04-08 18:24:58 -0700

    chnaged post to put for updating restaurants

--- project/backend/app/routers/restaurant_routers.py
+++ project/backend/app/routers/restaurant_routers.py
@@ -27,8 +27,8 @@ def post_restaurant(payload: RestaurantCreate):
 def get_restaurant(restaurant_id: int):
     return get_restaurant_by_id(restaurant_id=restaurant_id)
 
-@router.post("/{restaurant_id}", response_model=Restaurant, status_code=201)
-def post_restaurant_update(restaurant_id: int,payload: RestaurantUpdate):
+@router.put("/{restaurant_id}", response_model=Restaurant, status_code=200)
+def put_restaurant_update(restaurant_id: int, payload: RestaurantUpdate):
     return update_restaurant(restaurant_id, payload)
 
 @router.delete("/{restaurant_id}", response_model=None, status_code=200)
