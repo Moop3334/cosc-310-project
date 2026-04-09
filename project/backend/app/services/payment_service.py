fb9230b494462dbe3868e26106dd86a4062217ae
YugGull <yug.gedu@gmail.com>
2026-04-08 19:37:24 -0700

    changed Cart to display the credits

--- project/backend/app/services/payment_service.py
+++ project/backend/app/services/payment_service.py
@@ -3,8 +3,10 @@ from fastapi import HTTPException
 from app.schema.payment import Payment
 from app.schema.shopping_cart import CartItem
 
+gst = 1.05
+delivery_fee = 3
 def calculate_subtotal(items: List[CartItem]) -> float:
     return sum(item.price * item.quantity for item in items)
 
 def calculate_total(items: List[CartItem]) -> float:
-    return (calculate_subtotal(items) * 1.05) + 3 #Magic numbers, please change to literals to make it easier to understand
+    return (calculate_subtotal(items) * gst) + delivery_fee
